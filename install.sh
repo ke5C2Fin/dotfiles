a33c2d95e227c75a10267daddb61702569ce1de0
Kevin H <a@a>
2019-10-09 17:27:55 -0400

    dotfile - df

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 for file in dot_*; do
-    dotfile=${HOME}/.${file#dot_}
-    [ -f ${dotfile} ] && mv ${dotfile} ${dotfile}.orig
-    ln -sfv ${PWD}/${file} ${dotfile}
+    df=${HOME}/.${file#dot_}
+    [ -f ${df} ] && mv ${df} ${df}.orig
+    ln -sfv ${PWD}/${file} ${df}
 done
 
