1be3cbc6e818fb30ee23d15529498e6ddd520b69
Kevin H <a@a>
2019-10-10 09:52:40 -0400

    clutter

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/sh -
 
 for file in dot_*; do
-    df=${HOME}/.${file#dot_}
-    diff ${file} ${df} > /dev/null || mv -f ${df} ${df}.orig
-    ln -sfv ${PWD}/${file} ${df}
+    df=$HOME/.${file#dot_}
+    diff $file $df > /dev/null || mv -f $df $df.orig
+    ln -sfv $PWD/$file $df
 done
 
