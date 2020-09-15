6510c5516f9eadf9f68f85763d2a7ef2795fbec7
Kevin Harrington <kevin.harrington@caci.com>
2020-09-15 14:48:06 -0600

    <

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 for f in dot_*; do
     df=$HOME/.${f#dot_}
-    diff $f $df > /dev/null || mv -f $df $df.orig
+    diff $f $df >/dev/null || mv -f $df $df.orig
     ln -sfv $PWD/$f $df
 done
